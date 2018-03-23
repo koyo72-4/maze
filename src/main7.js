65ad477ccae541d89ec639076fb75fcca97922e6
Me <32035846+koyo72-4@users.noreply.github.com>
2018-03-22 21:17:51 -0400

    Commit.

--- src/main7.js
+++ src/main7.js
@@ -7,9 +7,9 @@ class Maze {
 
   static generateMaze() {
     let grid = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
       let row = [];
-      for (let j = 0; j < 10; j++) {
+      for (let j = 0; j < 20; j++) {
         row.push('o');
       }
       grid.push(row);
@@ -31,142 +31,428 @@ class Maze {
   }
 
   randomSlashes(i, rowIndex, columnIndex) {
-    while (i < 200) {
+    if (i >= 50) {
+      return;
+    }
+    if (!this._cache.left) {
+      console.log('first time');
       this.arraySlashes(i, rowIndex, columnIndex);
-      if (!this._cache[0]) {
-        let randomLeft = Math.floor(Math.random() * 2);
-        console.log(this._cache);
-        if (randomLeft === 0) {
-          this._cache.left = this._slashArray[i];
-          console.log(this._cache.left[0]);
-          i++;
-          this.arraySlashes(i, rowIndex + 2, columnIndex);
-          this._cache.right = this._slashArray[i];
-          console.log(this._cache);
-        } else if (randomLeft === 1) {
-          this._cache.right = this._slashArray[i];
-          console.log(this._cache.right[0]);
-          i++;
-          this.arraySlashes(i, rowIndex - 2, columnIndex);
-          this._cache.left = this._slashArray[i];
-          console.log(this._cache);
-        }
+      let randomLeft = Math.floor(Math.random() * 2);
+      console.log(this._cache);
+      if (randomLeft === 0) {
+        this._cache.left = this._slashArray[i];
+        console.log(this._cache.left[0]);
         i++;
+        this.arraySlashes(i, rowIndex + 2, columnIndex);
+        this._cache.right = this._slashArray[i];
+        console.log(this._cache);
+      } else if (randomLeft === 1) {
+        this._cache.right = this._slashArray[i];
+        console.log(this._cache.right[0]);
+        i++;
+        this.arraySlashes(i, rowIndex - 2, columnIndex);
+        this._cache.left = this._slashArray[i];
+        console.log(this._cache);
       }
-      let randomTurn = Math.floor(Math.random() * 3);
-      if (randomTurn === 0) {
-        if (this._cache.left[0] < this._cache.right[0]) {
-          rowIndex = this._cache.left[0];
-          columnIndex = this._cache.left[1] + 1;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          this._cache.left = this._slashArray[i];
-          i++;
-          rowIndex = this._cache.right[0];
-          columnIndex = this._cache.right[1] + 1;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          this._cache.right = this._slashArray[i];
-          i++;
-          console.log('slash array:');
-          console.log(this._slashArray);
-          console.log('cache:');
-          console.log(this._cache);
-          return;
-        } else {
-          return;
-        }
-      } else if (randomTurn === 1) {
-        let a;
-        let b;
-        let c;
-        let d;
-        if (this._cache.left[0] < this._cache.right[0]) {
-          a = -1;
-          b = 0;
-          c = 1;
-          d = 2;
+      i++;
+    }
+    console.log('before randomTurn');
+    let randomTurn = Math.floor(Math.random() * 3);
+    if (randomTurn === 0) {
+      console.log('randomTurn: straight: ' + randomTurn);
+      if (this._cache.left[0] < this._cache.right[0]) {
+        console.log('this._cache.left[0] < this._cache.right[0]');
+        rowIndex = this._cache.left[0];
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.left[1] + 1;
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.left = this._slashArray[i];
+        i++;
+        rowIndex = this._cache.right[0];
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.right[1] + 1;
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.right = this._slashArray[i];
+        i++;
+        console.log('slash array:');
+        console.log(this._slashArray);
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[0] > this._cache.right[0]) {
+        console.log('this._cache.left[0] > this._cache.right[0]');
+        rowIndex = this._cache.left[0];
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.left[1] - 1;
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.left = this._slashArray[i];
+        i++;
+        rowIndex = this._cache.right[0];
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.right[1] - 1;
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.right = this._slashArray[i];
+        i++;
+        console.log('slash array:');
+        console.log(this._slashArray);
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[1] < this._cache.right[1]) {
+        console.log('this._cache.left[1] < this._cache.right[1]');
+        rowIndex = this._cache.left[0] - 1;
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.left[1];
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.left = this._slashArray[i];
+        i++;
+        rowIndex = this._cache.right[0] - 1;
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.right[1];
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.right = this._slashArray[i];
+        i++;
+        console.log('slash array:');
+        console.log(this._slashArray);
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[1] > this._cache.right[1]) {
+        console.log('this._cache.left[1] > this._cache.right[1]');
+        rowIndex = this._cache.left[0] + 1;
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.left[1];
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.left = this._slashArray[i];
+        i++;
+        rowIndex = this._cache.right[0] + 1;
+        console.log('rowIndex:');
+        console.log(rowIndex);
+        columnIndex = this._cache.right[1];
+        console.log('columnIndex:');
+        console.log(columnIndex);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        this._cache.right = this._slashArray[i];
+        i++;
+        console.log('slash array:');
+        console.log(this._slashArray);
+        console.log('cache:');
+        console.log(this._cache);
+      }
+    } else if (randomTurn === 1) {
+      console.log('randomTurn: left: ' + randomTurn);
+      let a;
+      let b;
+      let c;
+      let d;
+      if (this._cache.left[0] < this._cache.right[0]) {
+        console.log('this._cache.left[0] < this._cache.right[0]');
+        a = -1;
+        b = 0;
+        c = 1;
+        d = 2;
 
-          rowIndex = this._cache.left[0] + a;
-          columnIndex = this._cache.left[1] + b;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.left[0] + a;
+        columnIndex = this._cache.left[1] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.right[0] + b;
-          columnIndex = this._cache.right[1] + c;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.right[0] + b;
+        columnIndex = this._cache.right[1] + c;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          columnIndex = this._cache.right[1] + d;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        columnIndex = this._cache.right[1] + d;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.left[0] + c;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.left[0] + c;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.left[0] + b;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.left[0] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.left[0] + a;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.left[0] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          this._cache.left = this._slashArray[i - 6];
-          this._cache.right = this._slashArray[i - 1];
-          console.log('cache:');
-          console.log(this._cache);
+        this._cache.left = this._slashArray[i - 6];
+        this._cache.right = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
 
-          return;
-        } else {
-          return;
-        }
-      } else if (randomTurn === 2) {
-        let a;
-        let b;
-        let c;
-        if (this._cache.left[0] < this._cache.right[0]) {
-          a = 1;
-          b = 0;
-          c = 2;
+      } else if (this._cache.left[1] > this._cache.right[1]) {
+        console.log('this._cache.left[1] > this._cache.right[1]');
+        a = 0;
+        b = 1;
+        c = 2;
+        d = -1;
 
-          rowIndex = this._cache.right[0] + a;
-          columnIndex = this._cache.right[1] + b;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.left[0] + a;
+        columnIndex = this._cache.left[1] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.left[0] + b;
-          columnIndex = this._cache.left[1] + a;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.right[0] + b;
+        columnIndex = this._cache.right[1] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          columnIndex = this._cache.left[1] + c;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        rowIndex = this._cache.right[0] + c;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.left[0] + a;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        columnIndex = this._cache.right[1] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.right[0] + b;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        columnIndex = this._cache.left[1] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          rowIndex = this._cache.right[0] + a;
-          this.arraySlashes(i, rowIndex, columnIndex);
-          i++;
+        columnIndex = this._cache.left[1] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
 
-          this._cache.right = this._slashArray[i - 6];
-          this._cache.left = this._slashArray[i - 1];
-          console.log('cache:');
-          console.log(this._cache);
+        this._cache.left = this._slashArray[i - 6];
+        this._cache.right = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
 
-          return;
-        } else {
-          return;
-        }
+      } else if (this._cache.left[0] > this._cache.right[0]) {
+        console.log('this._cache.left[0] > this._cache.right[0]');
+
+        rowIndex = this._cache.left[0] + 1;
+        columnIndex = this._cache.left[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + 0;
+        columnIndex = this._cache.right[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + (-2);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + 1;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + 1;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.left = this._slashArray[i - 6];
+        this._cache.right = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[1] < this._cache.right[1]) {
+        console.log('this._cache.left[1] < this._cache.right[1]');
+
+        rowIndex = this._cache.left[0] + 0;
+        columnIndex = this._cache.left[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + (-1);
+        columnIndex = this._cache.right[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + (-2);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.left = this._slashArray[i - 6];
+        this._cache.right = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
+      }
+    } else if (randomTurn === 2) {
+      console.log('randomTurn: right: ' + randomTurn);
+      let a;
+      let b;
+      let c;
+      if (this._cache.left[0] < this._cache.right[0]) {
+        console.log('this._cache.left[0] < this._cache.right[0]');
+        a = 1;
+        b = 0;
+        c = 2;
+
+        rowIndex = this._cache.right[0] + a;
+        columnIndex = this._cache.right[1] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + b;
+        columnIndex = this._cache.left[1] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + c;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + b;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + a;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.right = this._slashArray[i - 6];
+        this._cache.left = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
+
+      } else if (this._cache.left[0] > this._cache.right[0]) {
+        console.log('this._cache.left[0] > this._cache.right[0]');
+
+        rowIndex = this._cache.right[0] - 1;
+        columnIndex = this._cache.right[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + 0;
+        columnIndex = this._cache.left[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + (-2);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.right[0] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.right = this._slashArray[i - 6];
+        this._cache.left = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[1] < this._cache.right[1]) {
+        console.log('this._cache.left[1] < this._cache.right[1]');
+
+        rowIndex = this._cache.right[0] + 0;
+        columnIndex = this._cache.right[1] + 1;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + (-1);
+        columnIndex = this._cache.left[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + (-2);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + 1;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + 1;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.right = this._slashArray[i - 6];
+        this._cache.left = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
+      } else if (this._cache.left[1] > this._cache.right[1]) {
+        console.log('this._cache.left[1] > this._cache.right[1]');
+
+        rowIndex = this._cache.right[0] + 0;
+        columnIndex = this._cache.right[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + 1;
+        columnIndex = this._cache.left[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        rowIndex = this._cache.left[0] + 2;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.left[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + 0;
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        columnIndex = this._cache.right[1] + (-1);
+        this.arraySlashes(i, rowIndex, columnIndex);
+        i++;
+
+        this._cache.right = this._slashArray[i - 6];
+        this._cache.left = this._slashArray[i - 1];
+        console.log('cache:');
+        console.log(this._cache);
       }
     }
-
+    console.log('i: ' + i);
+    console.log('rowIndex: ' + rowIndex);
+    console.log('columnIndex: ' + columnIndex);
+    this.randomSlashes(i, rowIndex, columnIndex);
   }
 
 }
@@ -174,4 +460,4 @@ class Maze {
 const maze = new Maze();
 console.log(maze._mazeGrid);
 maze.print();
-maze.randomSlashes(0, 4, 0);
+maze.randomSlashes(0, 10, 0);
